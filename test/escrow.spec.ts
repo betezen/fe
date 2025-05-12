0bafdc7d67c497a3007a95da20e71b89ed088714
Mona Lisa <ajiguruhprasetyo@gmail.com>
2025-05-12 11:50:22 +0700

    add init anchor rust

---
+++ test/escrow.spec.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
+
+// describe('escrow', () => {
+
+// });
