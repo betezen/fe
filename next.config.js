6063d4e346aaf030224e3cf8978e722865e4a85c
syahlevi-aldanra <ruchapnara@gmail.com>
2025-05-13 09:26:00 +0700

    fix2

---
+++ next.config.js
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+        pathname: "**",
+      },
+    ],
+    domains: ["i.pravatar.cc", "flagcdn.com"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    optimizeCss: false,
+  },
+  webpack: (config) => {
+    // Skip lightningcss native module
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      lightningcss: false,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
